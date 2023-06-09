bd8e7a85f002dc60d9dc1da6484eb3f96f308d24
rogepi <rogepi@outlook.com>
2023-06-09 16:09:34 +0800

    ✨ feat: update blog page

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -1,16 +1,30 @@
 import { allPosts } from 'contentlayer/generated'
+import { format, parseISO } from 'date-fns'
+import { getMDXComponent } from 'next-contentlayer/hooks'
 
-export default function PostPage({ params }: { params: { slug: string } }) {
+export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
+
+export const generateMetadata = ({ params }:{ params: { slug: string }}) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
+  return { title: post.title }
+}
+
+const PostLayout = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+
+  const Content = getMDXComponent(post.body.code)
 
   return (
-    <article>
-      <header>{post.title}</header>
-      <div
-        className="[&>*:last-child]:mb-0 [&>*]:mb-3"
-        dangerouslySetInnerHTML={{ __html: post.body.code }}
-      />
+    <article className="mx-auto max-w-xl py-8">
+      <div className="mb-8 text-center">
+        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+        <h1>{post.title}</h1>
+      </div>
+      <Content />
     </article>
   )
 }
+
+export default PostLayout
