54096126b3ba4c86e2be36b36dc7d2f7a696f8d4
rogepi <rogepi@outlook.com>
2023-06-10 17:55:13 +0800

    ✨ feat: update st

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -4,9 +4,10 @@ import { notFound } from 'next/navigation'
 import { getMDXComponent } from 'next-contentlayer/hooks'
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
-export const generateMetadata = ({ params }:{ params: { slug: string }}) => {
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
   return { title: post?.title }
 }
@@ -14,7 +15,7 @@ export const generateMetadata = ({ params }:{ params: { slug: string }}) => {
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
 
-  if(!post) {
+  if (!post) {
     notFound()
   }
 
