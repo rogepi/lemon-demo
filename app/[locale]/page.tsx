82dc66b3938fba5190ec7ad7db112533010fc655
rogepi <rogepi@outlook.com>
2023-06-12 00:42:32 +0800

    ✨ feat: update sth

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,8 @@
-import { useTranslations } from 'next-intl'
-
 import { CardsFlow } from '@/components/cards-flow'
 
 export default function RootPage() {
-  const t = useTranslations('Index')
   return (
     <div>
-      <h1>{t('title')}</h1>
       <CardsFlow />
     </div>
   )
