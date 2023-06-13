4e1c7a2ed9a952eabf3f500a74fa53600c461bd4
rogepi <rogepi@outlook.com>
2023-06-13 18:25:56 +0800

    ✨ feat: add navBar an

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,46 +1,71 @@
 'use client'
 
+import { LayoutGroup, motion } from 'framer-motion'
 import { useTranslations } from 'next-intl'
 import { usePathname } from 'next-intl/client'
 import Link from 'next-intl/link'
 import * as React from 'react'
 
 import { MAIN_NAV_ITEMS } from '@/config/nav'
+import { cn } from '@/lib/utils'
 
-type NavItemType = (typeof MAIN_NAV_ITEMS)[number]
-
-const NavItem = ({ item }: { item: NavItemType }) => {
-  const t = useTranslations('Nav')
-  const pathName = usePathname()
-
-  const isActive =
-    item.key === 'home' ? pathName === '/' : pathName.startsWith(item.url)
-
+const NavItem = ({
+  url,
+  text,
+  isActive,
+}: {
+  url: string
+  text: string
+  isActive: boolean
+}) => {
   return (
-    <Link href={item.url}>
-      <div
-        className={`rounded-full p-1 px-2 text-sm font-semibold ${
-          isActive
-            ? ` bg-gray-50  dark:bg-zinc-500`
-            : 'hover:bg-gray-100 dark:hover:bg-zinc-600'
-        }`}
-      >
-        {t(`${item.key as 'home' | 'blog' | 'question' | 'about'}`)}
+    <Link href={url}>
+      <div className={cn('z-auto rounded-full p-2 text-sm font-semibold')}>
+        <span className="relative p-2 px-3">
+          <span className="isolate z-50 dark:text-white">{text}</span>
+          {isActive ? (
+            <motion.div
+              className="absolute inset-0 z-[-1] rounded-full bg-gray-50 dark:bg-zinc-500"
+              layoutId="navBar"
+              transition={{
+                type: 'spring',
+                stiffness: 350,
+                damping: 30,
+              }}
+            />
+          ) : null}
+        </span>
       </div>
     </Link>
   )
 }
 
 export function NavBar({ className }: { className?: string }) {
+  const t = useTranslations('Nav')
+  let pathname = usePathname() || '/'
+  if (pathname.includes('/blog/')) {
+    pathname = '/blog'
+  }
   return (
-    <nav className={className}>
-      <ul className="flex items-center gap-5 rounded-full bg-zinc-200/75 p-1 dark:bg-zinc-700">
-        {MAIN_NAV_ITEMS.map((item, index) => (
-          <li key={index}>
-            <NavItem item={item} />
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <LayoutGroup>
+      <nav className={className}>
+        <ul className=" relative z-10 flex items-center gap-5 rounded-full bg-zinc-200/75 p-1 dark:bg-zinc-700">
+          {MAIN_NAV_ITEMS.map((item, index) => {
+            const isActive = item.url === pathname
+            return (
+              <li key={index}>
+                <NavItem
+                  url={item.url}
+                  text={t(
+                    `${item.key as 'home' | 'blog' | 'question' | 'about'}`
+                  )}
+                  isActive={isActive}
+                />
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    </LayoutGroup>
   )
 }
